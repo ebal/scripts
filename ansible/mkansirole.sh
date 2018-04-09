dc7aae2ca647873981471a8b2c745f18bedd9a81
Evaggelos Balaskas <github.com@disposable.space>
2018-04-09 12:24:31 +0300

    Create mkansirole.sh

---
+++ ansible/mkansirole.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# ebal, Mon, 09 Apr 2018 12:21:37 +0300
+a bash shell script for creating a directory structure within the Ansible role templates (with main.yml)
+
+echo " "
+
+function die() {
+    exit 1
+}
+
+function usage() {
+    echo -e "USAGE: mkansirole rolename\n"
+    die
+}
+
+function check_args() {
+    if [ $# -ne 1 ]
+      then
+        usage
+    fi
+}
+
+function create_role() {
+    role=$1
+    DIRS=(tasks handlers files templates vars defaults meta)
+
+    for i in ${DIRS[@]}; do
+        mkdir -p ${role}/$i;
+        touch ${role}/$i/main.yml;
+    done
+    
+    cat <<EOF > ./${role}/tasks/main.yml 
+---
+  - name: Debug that our ansible role is working
+    debug:
+      msg: "It Works !"
+EOF
+
+}
+
+function create_playbook(){
+    role=$1.yml
+
+    cat <<EOF > ./${role}
+---
+- hosts: all
+  gather_facts: no
+  roles:
+    - role: $1
+EOF
+}
+
+function main() {
+    check_args "$0"
+    create_role "$1"
+    create_playbook "$1"
+    tree "$1"
+}
+
+main "$@"
