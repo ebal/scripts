461ae7f1510d43733e9ff033ea1b95b6808c9a84
Evaggelos Balaskas <github.com@disposable.space>
2018-06-03 23:20:42 +0300

    Update mkansirole.sh

--- ansible/mkansirole.sh
+++ ansible/mkansirole.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+
 # ebal, Mon, 09 Apr 2018 12:21:37 +0300
-a bash shell script for creating a directory structure within the Ansible role templates (with main.yml)
+# a bash shell script for creating a directory structure within the Ansible role templates (with main.yml)
 
 echo " "
 
@@ -34,6 +35,11 @@ function create_role() {
   - name: Debug that our ansible role is working
     debug:
       msg: "It Works !"
+
+  - name: Install the Extra Packages for Enterprise Linux repository
+    yum:
+      name: epel-release
+      state: present
 EOF
 
 }
@@ -55,6 +61,9 @@ function main() {
     create_role "$1"
     create_playbook "$1"
     tree "$1"
+    echo ""
+    echo -e "Run: 'ansible-playbook "$1".yml -c local -l localhost'"
+    echo ""
 }
 
 main "$@"
