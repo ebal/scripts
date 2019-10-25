2bc782f63a0775a49987fe17f0801696b22aeede
Evaggelos Balaskas <evaggelos [_at_] balaskas [_dot_] gr>
2019-10-26 01:26:42 +0300

    Update runme.sh & metrics

--- doh/metrics/runme.sh
+++ doh/metrics/runme.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-awk '{print "/usr/bin/time -o top100 -a -f %E curl -s  https://doh.libredns.gr/dns-query?name="$1" > /tmp/foobar"}' list | sh
+# ebal, Sat, 26 Oct 2019 00:42:26 +0300
+timestamp=`date +%s`
 
-for i in `seq 1000`; do /usr/bin/time -o 1000.times -a -f %E curl -s  https://doh.libredns.gr/dns-query?name=libredns.gr > /tmp/foobar; done
+for i in $(cat list); do
+    /usr/bin/time -o top100.$timestamp -a -f %E curl -s https://doh.libredns.gr/dns-query?name=$i > /dev/null;
+done
+
+for i in `seq 1000`; do
+    /usr/bin/time -o 1000.times.$timestamp -a -f %E curl -s  https://doh.libredns.gr/dns-query?name=libredns.gr > /dev/null;
+done
