44d6fdb6196197b8f424e9927afbe9c361cc3d6b
Evaggelos Balaskas <3bal_gpg@disposable.space>
2019-12-16 19:55:17 +0200

    Print Usage when arguments are not correct

--- python3/RFC.8484/RFC.8484.py
+++ python3/RFC.8484/RFC.8484.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
-# ./RFC.8484.py balaskas.gr AAAAA
-
 import dns.message
-import base64
 import requests
+import base64
 import sys
+
+if len (sys.argv) != 3 :
+    print ("Usage: python3 RFC.8484.py balaskas.gr AAAA")
+    sys.exit (1)
 
 message = dns.message.make_query( sys.argv[1] , sys.argv[2] )
 dns_req = base64.b64encode(message.to_wire()).decode('UTF8').rstrip('=')
 
 url = "https://doh.libredns.gr/dns/?dns=" + dns_req
 
-r = requests.get( url,
-        headers={"Content-type": "application/dns-message"}
-    )
+r = requests.get( url, headers={"Content-type": "application/dns-message"} )
 
 for answer in dns.message.from_wire( r.content ).answer:
     print(answer.to_text().split()[-1])
