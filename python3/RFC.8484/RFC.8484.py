9294f00e83c6d7a17b2636daefb60cffde0b1197
Evaggelos Balaskas <3bal_gpg@disposable.space>
2019-12-16 20:34:26 +0200

    Lint RFC.8484.py

--- python3/RFC.8484/RFC.8484.py
+++ python3/RFC.8484/RFC.8484.py
@@ -7,22 +7,23 @@ import sys
 
 RRs = ('A', 'AAAA', 'CNAME')
 
-if len (sys.argv) != 3 :
-    print ("Usage: python3 RFC.8484.py balaskas.gr AAAA")
-    sys.exit (1)
+if len(sys.argv) != 3:
+    print("Usage: python3 RFC.8484.py balaskas.gr AAAA")
+    sys.exit(1)
 
 if sys.argv[2] not in RRs:
-    print ("Supported Reourse Records: A, AAAA & CNAME")
-    sys.exit (1)
+    print("Supported Reourse Records: A, AAAA & CNAME")
+    sys.exit(1)
 
-message = dns.message.make_query( sys.argv[1] , sys.argv[2] )
+message = dns.message.make_query(sys.argv[1], sys.argv[2])
 dns_req = base64.b64encode(message.to_wire()).decode('UTF8').rstrip('=')
 
 url = "https://doh.libredns.gr/dns?dns=" + dns_req
 # url = "https://cloudflare-dns.com/dns-query?dns=" + dns_req
 # url = "https://dns.google/dns-query?dns=" + dns_req
 
-r = requests.get( url, headers={"Content-type": "application/dns-message"} )
+r = requests.get(url, headers={"Content-type": "application/dns-message"})
 
-for answer in dns.message.from_wire( r.content ).answer:
+for answer in dns.message.from_wire(r.content).answer:
     print(answer.to_text().split()[-1])
+
